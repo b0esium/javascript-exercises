5b2a06a6d3a59b00d43e728c38a53706bf0f419f
b0esium <b0esium@protonmail.com>
2022-10-21 11:20:08 +0200

    Finish exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  let arr = string.split("");
+  let res = [];
+  for (letter of arr) {
+    res.unshift(letter);
+  }
+  return res.join("");
 };
 
 // Do not edit below this line
