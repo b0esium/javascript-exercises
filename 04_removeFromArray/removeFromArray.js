86b5be235ccfbe7987b9bb60dff9278d235c892c
b0esium <b0esium@protonmail.com>
2022-10-21 11:46:45 +0200

    Finish exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (arr, ...valuesToRemove) {
+  return arr.filter((value, index, arr) => {
+    return valuesToRemove.indexOf(value) == -1;
+  });
 };
 
 // Do not edit below this line
