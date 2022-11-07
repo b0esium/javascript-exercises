bc00e9bdf9a3ce79b94a5642da383eae09dd5617
b0esium <b0esium@protonmail.com>
2022-11-07 11:47:22 +0100

    Finish exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+const palindromes = function (phrase) {
+  let cleanPhrase = phrase
+    .toLowerCase()
+    .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()\s]/g, "");
 
+  let reversedPhrase = cleanPhrase.split("").reverse().join("");
+
+  return reversedPhrase == cleanPhrase;
 };
 
 // Do not edit below this line
