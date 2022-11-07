07804cc500ac21d76804a612453d89f7f6443062
b0esium <b0esium@protonmail.com>
2022-11-07 15:17:54 +0100

    Finish exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  let highestAge = 0;
+  let age = 0;
+  let oldest = {};
 
+  for (person of people) {
+    if (!person.yearOfDeath) {
+      const d = new Date();
+      age = d.getFullYear() - person.yearOfBirth;
+    } else {
+      age = person.yearOfDeath - person.yearOfBirth;
+    }
+
+    if (age > highestAge) {
+      highestAge = age;
+      oldest = person;
+    }
+  }
+
+  return oldest;
 };
 
 // Do not edit below this line
